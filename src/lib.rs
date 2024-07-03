4c4b035fba786eede406f8f1bda4018fc1bdfcd0
Jan Hohenheim <jan@hohenheim.ch>
2024-07-03 18:19:45 +0200

    Try out settings

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use bevy::prelude::*;
 
 /// Everything you need to use the Avian Pickup plugin.
 pub mod prelude {
-    pub use crate::AvianPickupPlugin;
+    //pub use crate::{AvianPickupCamera, AvianPickupPlugin};
 }
 
 #[derive(Debug, Clone, Default, PartialEq)]
@@ -16,14 +16,27 @@ pub mod prelude {
 /// # Example
 ///
 /// ```no_run
-/// # use avian3d::prelude::*;
-/// # use avian_pickup::prelude::*;
-/// # use bevy::prelude::*;
+/// //# use avian3d::prelude::*;
+/// //# use avian_pickup::prelude::*;
+/// //# use bevy::prelude::*;
 ///
-/// App::new().add_plugins((DefaultPlugins, PhysicsPlugins::default(), AvianPickupPlugin::default()));
+/// //App::new().add_plugins((DefaultPlugins, PhysicsPlugins::default(), AvianPickupPlugin::default()));
 /// ```
 pub struct AvianPickupPlugin;
 
 impl Plugin for AvianPickupPlugin {
     fn build(&self, _app: &mut App) {}
 }
+
+#[derive(Debug, Clone, Copy, Hash, Component, Default, PartialEq, Eq, Reflect)]
+#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serialize", reflect(Serialize, Deserialize))]
+#[reflect(Debug, Component, Default, Hash, PartialEq)]
+/// Tag component for the camera that will be used for picking up objects.
+/// Place this on the camera entity that is under the player control.
+///
+/// # Example
+/// ```
+/// use avian_pickup::prelude::*;
+/// ```
+pub struct AvianPickupCamera;
