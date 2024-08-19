b6ae31a946ef077e0c66c6c17bb68dbdd3965dc0
Jan Hohenheim <jan@hohenheim.ch>
2024-08-19 21:04:05 +0200

    Implement weird stuff

--- src/interaction/hold/update.rs
+++ src/interaction/hold/update.rs
@@ -76,18 +76,38 @@ pub(super) fn update_object(
         let actor_pitch = actor_rotation.to_euler(EulerRot::YXZ).1;
         let _actor_to_prop_pitch = actor_pitch.clamp(clamp_pitch.min, clamp_pitch.max);
         let forward = Transform::from_rotation(actor_rotation.0).forward();
-        let compound_collider =
-            rigid_body_compound_collider(prop, &q_collider_ancestor, &q_collider);
-        let Some(compound_collider) = compound_collider else {
+        // We can't cast a ray wrt an entire rigid body out of the box,
+        // so we manually collect all colliders in the hierarchy and
+        // construct a compound collider.
+        let prop_collider = rigid_body_compound_collider(prop, &q_collider_ancestor, &q_collider);
+        let Some(prop_collider) = prop_collider else {
             error!("Held prop does not have a collider in its hierarchy. Ignoring.");
             continue;
         };
-        let radial = collide_get_extent(&compound_collider, Vec3::ZERO, prop_rotation.0, -forward);
+        let prop_radius_wrt_direction =
+            collide_get_extent(&prop_collider, Vec3::ZERO, prop_rotation.0, -forward);
+        let actor_collider = rigid_body_compound_collider(actor, &q_collider_ancestor, &q_collider);
+        let Some(actor_collider) = actor_collider else {
+            error!("AvianPickupActor does not have a collider in its hierarchy. Ignoring.");
+            continue;
+        };
+        let actor_radius_wrt_direction =
+            collide_get_extent(&actor_collider, Vec3::ZERO, actor_rotation.0, forward);
+
+        let min_distance = prop_radius_wrt_direction + actor_radius_wrt_direction;
+        // The 2013 code now additionally does `min_distance = (min_distance * 2) + 24
+        // inches` That seems straight up bizarre, so I refuse to do that.
+        let preferred_distance = preferred_distance.copied().unwrap_or_default().0;
+        // The 2013 code does `distance = preferred_distance + min_distance``
+        // which means that `preferred_distance` is the distance between the prop's
+        // edge and the actors's edge. Not wrong, but I think it's more intuitive
+        // to have the preferred distance be the distance between the prop's and
+        // actor's origins.
+        let distance = preferred_distance.max(min_distance);
 
         let _target_rotation = preferred_rotation
             .map(|preferred| preferred.0)
             .unwrap_or(prop_rotation.0);
-        let _target_distance = preferred_distance.copied().unwrap_or_default().0;
     }
 }
 
@@ -95,9 +115,10 @@ pub(super) fn update_object(
 /// but we can only do that for convex shapes in parry. Notably, compound shapes
 /// made of convex shapes are not supported.\
 /// So, we instead just cast a ray in the direction and get the hit point.
-/// Since the original code multiplies the directoin by the dot product of
+/// Since the original code multiplies the direction by the dot product of
 /// the direction and the support point, it looks like the result is the same.
-fn collide_get_extent(collider: &Collider, origin: Vec3, rotation: Quat, dir: Dir3) -> Vec3 {
+/// That's why we just return the TOI directly.
+fn collide_get_extent(collider: &Collider, origin: Vec3, rotation: Quat, dir: Dir3) -> f32 {
     const TRANSLATION: Vec3 = Vec3::ZERO;
     // We cast from inside the collider, so we don't care about a max TOI
     const MAX_TOI: f32 = f32::INFINITY;
@@ -109,5 +130,5 @@ fn collide_get_extent(collider: &Collider, origin: Vec3, rotation: Quat, dir: Di
         This means the compound collider we constructed is malformed.\n\
         This is a bug. Please report it on `avian_pickup`s GitHub page.",
     );
-    dir * toi
+    toi
 }
