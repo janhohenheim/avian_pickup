81e7d338f30acf65deb4b6540ab0732ccae9a346
Jan Hohenheim <jan@hohenheim.ch>
2024-08-20 03:32:23 +0200

    Implement graceful dropping

--- src/interaction/drop.rs
+++ src/interaction/drop.rs
@@ -8,13 +8,28 @@ pub(super) fn plugin(app: &mut App) {
 
 fn drop(
     mut commands: Commands,
-    mut q_state: Query<(Entity, &mut AvianPickupActorState, &mut Cooldown, &Dropping)>,
+    mut q_actor: Query<(Entity, &mut AvianPickupActorState, &mut Cooldown, &Dropping)>,
+    mut q_prop: Query<(
+        &mut Mass,
+        &mut LinearVelocity,
+        &mut AngularVelocity,
+        Option<&NonPickupMass>,
+    )>,
 ) {
-    for (actor, mut state, mut cooldown, drop) in q_state.iter_mut() {
-        let _prop = drop.0;
+    for (actor, mut state, mut cooldown, drop) in q_actor.iter_mut() {
+        let prop = drop.0;
         *state = AvianPickupActorState::Idle;
-        info!("Drop!");
         cooldown.drop();
         commands.entity(actor).remove::<Dropping>();
+        // Safety: the prop is a dynamic rigid body and thus is guaranteed to have a
+        // mass, linvel, and angvel.
+        let (mut mass, mut velocity, mut angvel, non_pickup_mass) = q_prop.get_mut(prop).unwrap();
+        let Some(non_pickup_mass) = non_pickup_mass else {
+            error!("Failed to give a dropped prop its pre-pickup mass back. Ignoring.");
+            continue;
+        };
+        mass.0 = non_pickup_mass.0;
+        velocity.0 = Vec3::ZERO;
+        angvel.0 = Vec3::ZERO;
     }
 }
