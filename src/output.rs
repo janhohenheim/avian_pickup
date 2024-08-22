59bbabb48a3365424f940e0b1049dc25f359e191
Jan Hohenheim <jan@hohenheim.ch>
2024-08-22 03:07:14 +0200

    Add docs

--- src/output.rs
+++ src/output.rs
@@ -12,6 +12,9 @@ pub(super) mod prelude {
     pub use super::{PropDropped, PropThrown};
 }
 
+/// Event sent when a prop is thrown by an actor.
+/// This is meant for the user to lister to in order to play sound effects, etc.
+/// Sending this has no effect on the prop itself.
 #[derive(Event, Debug, Clone, Copy, PartialEq, Eq, Reflect)]
 #[reflect(Debug, PartialEq)]
 #[cfg_attr(
@@ -20,11 +23,18 @@ pub(super) mod prelude {
     reflect(Serialize, Deserialize)
 )]
 pub struct PropThrown {
+    /// The thrown prop.
     pub prop: Entity,
+    /// The actor that threw the prop.
     pub actor: Entity,
+    /// Whether the prop was held when thrown. If `false`, the prop was directly
+    /// thrown while an actor was looking at it without holding it up first.
     pub was_held: bool,
 }
 
+/// Event sent when a prop is dropped by an actor.
+/// This is meant for the user to lister to in order to play sound effects, etc.
+/// Sending this has no effect on the prop itself.
 #[derive(Event, Debug, Clone, Copy, PartialEq, Eq, Reflect)]
 #[reflect(Debug, PartialEq)]
 #[cfg_attr(
@@ -33,7 +43,12 @@ pub struct PropThrown {
     reflect(Serialize, Deserialize)
 )]
 pub struct PropDropped {
+    /// The dropped prop.
     pub prop: Entity,
+    /// The actor that dropped the prop.
     pub actor: Entity,
+    /// Whether the drop was forced to be dropped by being too far away from its
+    /// target location. If `false`, the prop was dropped by the actor's own
+    /// volition.
     pub forced: bool,
 }
