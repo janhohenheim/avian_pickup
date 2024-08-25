08bd56bc623c71948c6043f9992bd5a4d4a4ca9c
Jan Hohenheim <jan@hohenheim.ch>
2024-08-25 22:11:10 +0200

    WIP: manipulate prop

---
+++ examples/manipulate_prop_while_holding.rs
@@ -0,0 +1,167 @@
+use std::f32::consts::FRAC_PI_2;
+
+use avian3d::prelude::*;
+use avian_pickup::prelude::*;
+use bevy::{
+    app::RunFixedMainLoop,
+    color::palettes::tailwind,
+    input::mouse::MouseMotion,
+    prelude::*,
+    time::run_fixed_main_schedule,
+};
+use bevy_transform_interpolation::*;
+
+mod util;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            PhysicsPlugins::default(),
+            // Because we are moving the camera independently of the physics system,
+            // interpolation is needed to prevent jittering.
+            TransformInterpolationPlugin::interpolate_all(),
+            AvianPickupPlugin::default(),
+            // This is just here to make the example look a bit nicer.
+            util::plugin(util::Example::Generic),
+        ))
+        .add_systems(Startup, setup)
+        // Need to read input and rotate camera before physics,
+        // this is unfortunately the best way to schedule this:
+        // <https://github.com/bevyengine/bevy/issues/14873>
+        .add_systems(
+            RunFixedMainLoop,
+            (handle_input, rotate_camera).before(run_fixed_main_schedule),
+        )
+        .run();
+}
+
+/// Spawn the camera, light, ground, and a box to pick up.
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let terrain_material = materials.add(Color::WHITE);
+    let prop_material = materials.add(Color::from(tailwind::EMERALD_300));
+
+    commands.spawn((
+        Name::new("Player Camera"),
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 1.0, 5.0),
+            ..default()
+        },
+        // Add this to set up the camera as the entity that can pick up
+        // objects.
+        AvianPickupActor::default(),
+        // This entity is moved in a variable timestep, so no interpolation is needed.
+        NoRotationInterpolation,
+    ));
+
+    commands.spawn((
+        Name::new("Light"),
+        PointLightBundle {
+            transform: Transform::from_xyz(3.0, 8.0, 3.0),
+            point_light: PointLight {
+                color: Color::WHITE,
+                intensity: 2_000_000.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            ..default()
+        },
+    ));
+
+    let ground_shape = Cuboid::new(15.0, 0.25, 15.0);
+    commands.spawn((
+        Name::new("Ground"),
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(ground_shape)),
+            material: terrain_material.clone(),
+            ..default()
+        },
+        RigidBody::Static,
+        Collider::from(ground_shape),
+    ));
+
+    let box_shape = Cuboid::from_size(Vec3::splat(0.5));
+    commands.spawn((
+        Name::new("Box"),
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(box_shape)),
+            material: prop_material.clone(),
+            transform: Transform::from_xyz(0.0, 2.0, 3.5),
+            ..default()
+        },
+        // All `RigidBody::Dynamic` entities are able to be picked up.
+        RigidBody::Dynamic,
+        Collider::from(box_shape),
+    ));
+}
+
+/// Pass player input along to `avian_pickup`
+fn handle_input(
+    mut avian_pickup_input_writer: EventWriter<AvianPickupInput>,
+    key_input: Res<ButtonInput<MouseButton>>,
+    actors: Query<Entity, With<AvianPickupActor>>,
+) {
+    for actor in &actors {
+        if key_input.just_pressed(MouseButton::Left) {
+            avian_pickup_input_writer.send(AvianPickupInput {
+                kind: AvianPickupInputKind::JustPressedL,
+                actor,
+            });
+        }
+        if key_input.just_pressed(MouseButton::Right) {
+            avian_pickup_input_writer.send(AvianPickupInput {
+                kind: AvianPickupInputKind::JustPressedR,
+                actor,
+            });
+        }
+        if key_input.pressed(MouseButton::Right) {
+            avian_pickup_input_writer.send(AvianPickupInput {
+                kind: AvianPickupInputKind::PressedR,
+                actor,
+            });
+        }
+    }
+}
+
+fn rotate_camera(
+    time: Res<Time>,
+    mut mouse_motion: EventReader<MouseMotion>,
+    mut cameras: Query<&mut Transform, With<Camera>>,
+) {
+    for mut transform in &mut cameras {
+        let dt = time.delta_seconds();
+        // The factors are just arbitrary mouse sensitivity values.
+        // It's often nicer to have a faster horizontal sensitivity than vertical.
+        let mouse_sensitivity = Vec2::new(0.12, 0.10);
+
+        for motion in mouse_motion.read() {
+            let delta_yaw = -motion.delta.x * dt * mouse_sensitivity.x;
+            let delta_pitch = -motion.delta.y * dt * mouse_sensitivity.y;
+
+            // Add yaw (global)
+            transform.rotate_y(delta_yaw);
+
+            // Add pitch (local)
+            const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+            let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+            let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+            transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+        }
+    }
+}
+
+/// Systems in fixed timesteps may not run every frame,
+/// so we cache them 
+#[derive(Debug, Component, Default)]
+struct InputCache {
+    move_accumulator: i32,
+    shift_presset: bool,
+}
+
+fn move_prop(
+    caches: 
+)
