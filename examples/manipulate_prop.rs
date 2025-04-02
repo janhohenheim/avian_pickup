90164105d14ee9cf7a18e09eda4deb9a40f981d6
Jan Hohenheim <jan@hohenheim.ch>
2025-04-02 20:42:54 +0200

    Refactor camera rotation code

--- examples/manipulate_prop.rs
+++ examples/manipulate_prop.rs
@@ -10,7 +10,7 @@ use avian_pickup::{
 };
 use bevy::{
     color::palettes::tailwind,
-    input::mouse::{MouseMotion, MouseWheel},
+    input::mouse::{AccumulatedMouseMotion, MouseMotion, MouseWheel},
     prelude::*,
 };
 
@@ -129,22 +129,24 @@ fn handle_pickup_input(
     }
 }
 
-fn rotate_camera(mut cameras: Query<(&mut Transform, &mut InputAccumulation), With<Camera>>) {
-    for (mut transform, mut input) in &mut cameras {
-        if input.shift {
-            continue;
-        }
+fn rotate_camera(
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    mut transform: Single<&mut Transform, With<Camera>>,
+) {
+    // The factors are just arbitrary mouse sensitivity values.
+    let camera_sensitivity = Vec2::new(0.001, 0.001);
 
-        let delta_yaw = -input.rotation.x;
-        let delta_pitch = -input.rotation.y;
-        input.rotation = Vec2::ZERO;
+    let delta = accumulated_mouse_motion.delta;
+    let delta_yaw = -delta.x * camera_sensitivity.x;
+    let delta_pitch = -delta.y * camera_sensitivity.y;
 
-        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
-        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
-        let yaw = yaw + delta_yaw;
-        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
-        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
-    }
+    let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+    let yaw = yaw + delta_yaw;
+
+    const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+    let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+    transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
 }
 
 fn accumulate_input(
